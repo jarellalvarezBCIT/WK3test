f8f90c0652d43c3b2c4186aa669465be931e7f7a
jarellalvarezBCIT <jarell_alvarez@bcit.ca>
2024-09-13 12:07:18 -0700

    new branch

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ console.log("hey hey");
 console.log("new branch");
 console.log("new branch 2");
 const mainCharacter = "joe";
-console.log("my new friend is" + mainCharacter);
+console.log("my new friend is");
